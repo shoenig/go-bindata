f798076d1c76609965cdfd60cbcee1c5ac05dfd6
Gordon Schneider <1594377+ailurarctos@users.noreply.github.com>
2019-05-26 18:07:19 +0900

    Use the go:generate comment convention
    This convention is defined in
    https://golang.org/pkg/cmd/go/internal/generate/ as follows:
    
        ^// Code generated .* DO NOT EDIT\.$
    
    Also update testdata/Makefile so that it can regenerate and check
    the testdata/out/*.go files.
    
    Use the tools.go approach to define the lint/vet dependencies. This
    approach is described in
    https://github.com/go-modules-by-example/index/tree/23a56e1095864bf596f2ce3aec296ecc89ab71b9/010_tools
    and https://github.com/golang/go/issues/25922#issuecomment-451123151.

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/shoenig/petrify/v4
 
 go 1.12
+
+require (
+	github.com/kisielk/errcheck v1.2.0
+	golang.org/x/lint v0.0.0-20190409202823-959b441ac422
+	golang.org/x/tools v0.0.0-20190525145741-7be61e1b0e51
+)
---
+++ tools.go
@@ -0,0 +1,14 @@
+//+build tools
+
+// Package tools declares the tool dependencies required by petrify.
+//
+// This approach is described in
+// https://github.com/go-modules-by-example/index/tree/23a56e1095864bf596f2ce3aec296ecc89ab71b9/010_tools
+// and https://github.com/golang/go/issues/25922#issuecomment-451123151.
+package tools
+
+import (
+	_ "github.com/kisielk/errcheck"
+	_ "golang.org/x/lint/golint"
+	_ "golang.org/x/tools/go/analysis/passes/shadow/cmd/shadow"
+)
